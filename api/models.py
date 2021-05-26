7b6be8267ccefcebb3636b0d300ebb589964a776
DavinY <44568511+DavinY@users.noreply.github.com>
2021-05-27 03:22:07 +0700

    create CRUD for User

---
+++ api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    nama = models.CharField(max_length=255)
+    nik = models.CharField(max_length=16)
+    password = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    departemen = models.CharField(max_length=255)
+
+
+class Lapor(models.Model):
+    tanggel = models.DateTimeField()
+    judul = models.CharField(max_length=50)
+    deskripsi = models.CharField(max_length=255)
+    lokasi = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Create / Insert / Add - POST
+    # Retrieve /  Fetch - GET
+    # Update / Edit - PUT
+    # Delete / Remove - DELETE
--- japor_django/settings.py
+++ japor_django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'japor_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'japor_app',
+        'USER':'root',
+        'PASSWORD':'',
+
     }
 }
 
